939f2c31fa594c88c6e966714a05330f3426ecb2
Lukáš Runt <74490287+LRunt@users.noreply.github.com>
2021-09-26 00:04:48 +0200

    pridana trida vytvarejici kone

---
+++ src/Kun.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lukas Runt
+ * @version 1.0 (25-09-2021)
+ */
+public class Kun {
+	
+	/** Souradnice kone x*/
+	private int x;
+	/** Souradnice kone y*/
+	private int y;
+	/** hmotnost kone s vybavenim */
+	private int m;
+	/** doba nalozeni kone */
+	private int n;
+
+	/**
+	 * Konstruktor kone
+	 * @param x - x-ova souradnice
+	 * @param y - y-ova souradnice
+	 * @param m - hmotnost kone
+	 * @param n - doba nalozeni
+	 */
+	public Kun(int x, int y, int m, int n) {
+		this.x = x;
+		this.y = y;
+		this.m = m;
+		this.n = n;
+	}
+
+}
